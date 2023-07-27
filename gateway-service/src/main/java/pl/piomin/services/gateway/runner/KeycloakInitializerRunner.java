a8facbbd004ba1799c727362d7cbeba3cc2a4741
skyglass <skyglass2001@gmail.com>
2023-07-27 22:39:52 +0200

    try to fix

--- gateway-service/src/main/java/pl/piomin/services/gateway/runner/KeycloakInitializerRunner.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/runner/KeycloakInitializerRunner.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
-@Component
+//@Component
 public class KeycloakInitializerRunner implements CommandLineRunner {
 
     @Autowired
--- gateway-service/src/main/java/pl/piomin/services/gateway/security/WebSecurityConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/WebSecurityConfig.java
@@ -1,66 +1,77 @@
 package pl.piomin.services.gateway.security;
 
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
-import javax.net.ssl.SSLException;
-
-@RequiredArgsConstructor
+@Configuration
 @EnableWebFluxSecurity
 public class WebSecurityConfig {
 
-    private final JwtAuthConverter jwtAuthConverter;
-
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
-        http.authorizeExchange(exchange -> exchange
-                .pathMatchers(HttpMethod.GET, "/employee", "/employee/**").permitAll()
-                .pathMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .pathMatchers("/employee", "/employee/**").hasRole(ERP_MANAGER)
-                .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", "/", "/**",
-                        "/v3/api-docs/swagger-config", "/department/v3/api-docs", "/employee/v3/api-docs",
-                        "/organization/v3/api-docs",
-                        "/webjars", "/webjars/**",
-                        "/webjars/swagger-ui", "/webjars/swagger-ui/**").permitAll()
-                .anyExchange().authenticated());
-        http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthConverter));
-        //http.sessionManager().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().and().csrf().disable();
-        return http.build();
+    ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).jwsAlgorithm(SignatureAlgorithm.RS256).build();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.GET, "/employee", "/employee/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .pathMatchers("/employee", "/employee/**").hasRole(ERP_MANAGER)
+                        .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", "/", "/**",
+                                "/v3/api-docs/swagger-config", "/department/v3/api-docs", "/employee/v3/api-docs",
+                                "/organization/v3/api-docs",
+                                "/webjars", "/webjars/**",
+                                "/webjars/swagger-ui", "/webjars/swagger-ui/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
+                .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
+    }
+
+    @Bean
+    WebFilter csrfWebFilter() {
+        // Required because of https://github.com/spring-projects/spring-security/issues/5766
+        return (exchange, chain) -> {
+            exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
+                Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+                return csrfToken != null ? csrfToken.then() : Mono.empty();
+            }));
+            return chain.filter(exchange);
+        };
     }
 
     public static final String ERP_MANAGER = "ERP_MANAGER";
