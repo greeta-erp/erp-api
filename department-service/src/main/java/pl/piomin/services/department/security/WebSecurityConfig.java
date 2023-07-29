3147d55ca50310954efbdb562d8fd2f960a03667
skyglass <skyglass2001@gmail.com>
2023-07-29 16:32:12 +0200

    test

--- department-service/src/main/java/pl/piomin/services/department/security/WebSecurityConfig.java
+++ department-service/src/main/java/pl/piomin/services/department/security/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri,
                                  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String jwkIssuerUri) {
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).jwsAlgorithm(SignatureAlgorithm.RS256).build();
@@ -45,7 +45,7 @@ public class WebSecurityConfig {
             return claims;
         });
         return jwtDecoder;
-    }
+    }*/
 
     public static final String ERP_MANAGER = "ERP_MANAGER";
     public static final String USER = "USER";
