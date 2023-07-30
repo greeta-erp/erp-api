4a71772e3567d91410689533fa5f2337f73737a7
skyglass <skyglass2001@gmail.com>
2023-07-30 17:31:54 +0200

    cors

--- gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
@@ -1,30 +1,33 @@
 package pl.piomin.services.gateway.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-//@Configuration
+@Configuration
 //@EnableWebFlux
 public class CorsConfig { //implements WebFluxConfigurer {
-    /* @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOriginPatterns("*");
-    } */
+    @Value("${erp-app.redirect-url}")
+    private String erpAppRedirectUrl;
+
+    @Value("${movie-app.redirect-url}")
+    private String movieAppRedirectUrl;
 
     //@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOriginPattern("*"); // Allow requests from any origin. You may restrict this to specific origins in production.
-        corsConfig.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.).
-        corsConfig.addAllowedHeader("*"); // Allow all headers.
-        corsConfig.setAllowCredentials(true); // Allow credentials like cookies, if applicable.
+        corsConfig.addAllowedOriginPattern(erpAppRedirectUrl);
+        corsConfig.addAllowedOriginPattern(movieAppRedirectUrl);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
