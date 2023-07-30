50291ded66b3d87c78c3caf4baadc2887f9520e6
skyglass <skyglass2001@gmail.com>
2023-07-31 01:22:10 +0200

    cors

--- movie-service/src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
+++ movie-service/src/main/java/com/ivanfranchin/moviesapi/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,7 +33,7 @@ public class WebSecurityConfig {
                         jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //.cors(Customizer.withDefaults())
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
