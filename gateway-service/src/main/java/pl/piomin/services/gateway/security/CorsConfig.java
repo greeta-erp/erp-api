b94f7e04300380bc51e6a94a3d85ad89bd717006
skyglass <skyglass2001@gmail.com>
2023-07-30 15:51:14 +0200

    cors

--- gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
@@ -1,17 +1,12 @@
 package pl.piomin.services.gateway.security;
 
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebFluxConfigurer {
@@ -20,44 +15,17 @@ public class CorsConfig implements WebFluxConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedMethods("*")
-                .exposedHeaders(
-                        HttpHeaders.ORIGIN,
-                        HttpHeaders.SET_COOKIE,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
-                        HttpHeaders.CONTENT_TYPE,
-                        HttpHeaders.ACCEPT,
-                        HttpHeaders.AUTHORIZATION);
+                .allowedMethods("*");
     }
 
-    /*@Bean
+    @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
         UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-
         return new CorsWebFilter(corsConfigurationSource);
-    }*/
-
-    @Bean
-    public CorsWebFilter corsFilter() {
-        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-        //corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.setAllowedMethods(List.of("*"));
-        corsConfiguration.addAllowedHeader("origin");
-        corsConfiguration.addAllowedHeader("content-type");
-        corsConfiguration.addAllowedHeader("accept");
-        corsConfiguration.addAllowedHeader("authorization");
-        corsConfiguration.addAllowedHeader("cookie");
-        corsConfiguration.addAllowedHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(source);
     }
 }
