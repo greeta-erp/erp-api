859fc0f94b8d06abbd560b93b9738390f91a371e
skyglass <skyglass2001@gmail.com>
2023-07-30 02:53:07 +0200

    fix movie ui

--- gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
@@ -1,41 +1,36 @@
-package com.ivanfranchin.moviesapi.security;
+package pl.piomin.services.gateway.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 @Configuration
 public class CorsConfig implements WebFluxConfigurer {
-
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
                 .allowedOrigins("*")
+                .allowedHeaders("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .exposedHeaders(HttpHeaders.SET_COOKIE);
     }
 
     @Bean
-    public CorsWebFilter corsWebFilter(@Value("${erp-app.redirect-url}") String erpAppRedirectUrl,
-                                       @Value("${movie-app.redirect-url}") String movieAppRedirectUrl) {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        /*config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(List.of("*"));
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");*/
-        config.applyPermitDefaultValues();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsWebFilter(source);
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(corsConfigurationSource);
     }
 }
