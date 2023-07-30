e022fcbd1ffcd57afa861f7217d8fb2e7632b7d3
skyglass <skyglass2001@gmail.com>
2023-07-30 17:00:13 +0200

    cors

--- gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
@@ -3,34 +3,31 @@ package pl.piomin.services.gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableWebFlux
-public class CorsConfig implements WebFluxConfigurer {
-    @Override
+//@EnableWebFlux
+public class CorsConfig { //implements WebFluxConfigurer {
+    /* @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowedOriginPatterns("*");
-    }
+    } */
 
     @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //corsConfiguration.applyPermitDefaultValues();
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedOriginPattern("*");
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(corsConfigurationSource);
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); // Allow requests from any origin. You may restrict this to specific origins in production.
+        corsConfig.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.).
+        corsConfig.addAllowedHeader("*"); // Allow all headers.
+        corsConfig.setAllowCredentials(true); // Allow credentials like cookies, if applicable.
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
