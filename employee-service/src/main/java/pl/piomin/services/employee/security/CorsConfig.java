dc28f57a8b60f7a5e2c361ada321af08bbf82a57
skyglass <skyglass2001@gmail.com>
2023-07-31 17:59:12 +0200

    cors

--- employee-service/src/main/java/pl/piomin/services/employee/security/CorsConfig.java
+++ employee-service/src/main/java/pl/piomin/services/employee/security/CorsConfig.java
@@ -17,9 +17,10 @@ public class CorsConfig implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
     @Bean
